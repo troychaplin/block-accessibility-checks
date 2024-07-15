a2ff3ae7bb349f7ca662ffde04204dcdb0051ebc
Troy Chaplin <troy.chaplin@gmail.com>
2024-07-15 19:48:28 -0400

    add: button check for text and url

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '1255cfafb7ab5628379e');
+<?php return array('dependencies' => array('wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '7829ab3ceca32a3a3896');
