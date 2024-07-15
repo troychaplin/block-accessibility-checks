00ad5cb4ea58418c0841de2f463c5ea3dd29d577
Michael Corkum <mcorkum@gmail.com>
2024-07-15 15:18:12 -0400

    change: add composer, switch plugin logic to load in namespace

--- block-accessibility-checks.php
+++ block-accessibility-checks.php
@@ -1,8 +1,7 @@
 <?php
-
 /**
  * Plugin Name:       Block Accessibility Checks
- * Description:       This plugin helps ensures your content meets WCAG (Web Content Accessibility Guidelines) requirements.
+ * Description:       This plugin helps ensure your content meets WCAG (Web Content Accessibility Guidelines) requirements.
  * Requires at least: 6.3
  * Requires PHP:      7.0
  * Version:           0.1.0
@@ -14,24 +13,12 @@
  * @package           block-accessibility-checks
  */
 
-add_action('enqueue_block_editor_assets', 'enqueue_block_checks');
+// Setup autoloading
+require_once __DIR__ . '/vendor/autoload.php';
 
-function enqueue_block_checks()
-{
-    $script_path = 'build/block-checks.js';
-    $style_path = 'build/block-checks.css';
+// Include dependencies
+use BlockAccessibility\AssetEnqueuer;
 
-    wp_enqueue_script(
-        'block-checks-script',
-        plugins_url($script_path, __FILE__),
-        array('wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor'),
-        filemtime(plugin_dir_path(__FILE__) . $script_path),
-        true
-    );
-
-    wp_enqueue_style(
-        'block-checks-style',
-        plugins_url($style_path, __FILE__),
-        [],
-    );
-}
+// Enqueue block editor assets
+$assetEnqueuer = new AssetEnqueuer(__FILE__);
+add_action('enqueue_block_editor_assets', [$assetEnqueuer, 'enqueueAssets']);
