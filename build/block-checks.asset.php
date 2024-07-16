84e7189c33ac275bb24e7efee94bcd4fc5da570b
Troy Chaplin <troy.chaplin@gmail.com>
2024-07-15 20:16:36 -0400

    fix: publish locking

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '6b866d9b49d48f64fb2a');
+<?php return array('dependencies' => array('wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '2f3640e923011c92b85e');
