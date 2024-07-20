14bf6d51ecfd5ac4e89405d90fedf0f56cdb88d1
Troy Chaplin <troy.chaplin@gmail.com>
2024-07-20 09:03:42 -0400

    fix: commitlint config

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'ca5354eac98a908e207e');
+<?php return array('dependencies' => array('wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '2f3640e923011c92b85e');
