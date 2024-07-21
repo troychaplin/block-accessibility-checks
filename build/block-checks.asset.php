f2fe7d374a1b2a7306923b4052fb49af6aec129d
Troy Chaplin <troy.chaplin@gmail.com>
2024-07-20 23:27:28 -0400

    add: check for image alt text

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '2f3640e923011c92b85e');
+<?php return array('dependencies' => array('wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '6ec88ad3b3bd02d059fd');
--- src/scripts/registerPlugin.js
+++ src/scripts/registerPlugin.js
@@ -5,11 +5,13 @@ import './helpers/blockErrorComponent';
 // Import block check functions
 import { checkButtonAttributes } from './blockChecks/checkButton';
 import { checkHeadingLevel } from './blockChecks/checkHeading';
+import { checkImageAlt } from './blockChecks/checkImage';
 import { checkTableHeaderRow } from './blockChecks/checkTable';
 
 export const blockChecksArray = [
 	checkButtonAttributes,
 	checkHeadingLevel,
+	checkImageAlt,
 	checkTableHeaderRow,
 ];
 
