7c2a705b680b4ccd99e4883d17c32feaeb2bdcf7
Troy Chaplin <troy.chaplin@gmail.com>
2024-07-20 23:29:20 -0400

    change: build files

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '6ec88ad3b3bd02d059fd');
+<?php return array('dependencies' => array('wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '68a5860e88d2c3833c01');
