6b2113b559fcede058feca0775383b7ad7c9849e
Troy Chaplin <troy.chaplin@gmail.com>
2024-07-21 00:16:51 -0400

    change: error wrapper

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '7624461e9decf58da5c9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '2173d0cb3b5b7755029d');
