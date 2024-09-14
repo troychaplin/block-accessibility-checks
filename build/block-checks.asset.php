4349dc3e86bbcfd481a01a6814173d4f090d9b5e
Troy Chaplin <troy.chaplin@gmail.com>
2024-09-13 23:41:10 -0400

    add: clean build folder

---
+++ build/block-checks.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '39ddb813f352a8c5f281');
