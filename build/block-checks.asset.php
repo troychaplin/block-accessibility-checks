ca7c1f197ef4d67eea1c861280234cb0aa546124
Troy Chaplin <troy.chaplin@gmail.com>
2024-09-15 16:26:38 -0400

    change: editor error and warning styles

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'a8ef3cd57d13f1ba602f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'b408ac5230f941007a5c');
