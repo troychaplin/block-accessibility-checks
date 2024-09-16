34a024be499437ebd60f731df0451edf71ca4881
Troy Chaplin <troy.chaplin@gmail.com>
2024-09-16 17:06:57 -0400

    change: build files

--- build/block-admin.asset.php
+++ build/block-admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'a6c687c41cb9e5abe588');
+<?php return array('dependencies' => array(), 'version' => '8bf204609efefb27025d');
--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'bf49a7fc613abffa6ab9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'acda63614cbe439151ba');
