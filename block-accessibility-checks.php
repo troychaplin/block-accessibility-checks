c73d42b8298bcf2fc5f7fab9704b9ddc61125073
Troy Chaplin <15524465+troychaplin@users.noreply.github.com>
2024-10-06 11:03:07 -0400

    release: 1.0.2 (#45)

--- block-accessibility-checks.php
+++ block-accessibility-checks.php
@@ -5,7 +5,7 @@
  * Description:       Add errors and warnings to core blocks to meet WCAG (Web Content Accessibility Guidelines) requirements.
  * Requires at least: 6.3
  * Requires PHP:      7.0
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Troy Chaplin
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
