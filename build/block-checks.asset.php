5a2ed5a9e90b851e9228c08f3c1f00ef91bbe2c3
troychaplin <troy.chaplin@gmail.com>
2025-04-06 15:04:40 -0400

    add: changelog note

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '060edaa7f77667ba282d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'd32e5c19963fa887c910');
