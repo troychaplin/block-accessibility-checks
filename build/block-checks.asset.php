09d8a7a86a94053fd5f6616a33926fb4333807d2
troychaplin <troy.chaplin@gmail.com>
2025-04-13 19:48:35 -0400

    change: position of block message

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'aadb814e73dc1b33f3d8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '61440527cb25f0292cf0');
