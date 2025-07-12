f87def90093b97a3c6e25fa0480c7a046372dbec
troychaplin <troy.chaplin@gmail.com>
2025-07-12 09:15:38 -0230

    update: dependencies

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '61440527cb25f0292cf0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '8699bbf870e0ec07391e');
