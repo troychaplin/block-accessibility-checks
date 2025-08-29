1148e48fe120b38588f19037afd1740a6a2fdb39
Troy Chaplin <15524465+troychaplin@users.noreply.github.com>
2025-08-28 20:16:24 -0400

    change: panel title and message headings (#106)

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '263117f4c9e50a862130');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'd987f4caddf1c71829c5');
