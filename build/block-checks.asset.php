7edac120f11c37d59e50634e345b76f5b2945c60
Troy Chaplin <15524465+troychaplin@users.noreply.github.com>
2025-08-30 13:52:52 -0400

    Feature/multi block support (#109)
    * change: setting page label
    
    * change: styles
    
    * add: changelog notes

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '3135bafc52e91420ad8f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '3b3aeb8b94a8f61025ed');
