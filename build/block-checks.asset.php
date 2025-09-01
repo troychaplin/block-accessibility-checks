e47ccc357a6a0304af9856297fd6b62251e7d140
Troy Chaplin <15524465+troychaplin@users.noreply.github.com>
2025-09-01 09:42:34 -0400

    Fix/editor (#112)
    * fix: site editor loading
    
    * fix: conflict with site editor

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '3b3aeb8b94a8f61025ed');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'b336da62d5dfd07c3d44');
