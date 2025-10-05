476eb89cdb79ba589a7d8e857f29b56c2635406c
Troy Chaplin <15524465+troychaplin@users.noreply.github.com>
2025-10-05 11:10:47 -0400

    change: button text validation to check when no text charcters are used (#124)

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '362871c025d271f7094e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '70f205476b1771da15f1');
