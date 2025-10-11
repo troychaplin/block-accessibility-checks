9dd5da93969232124018b38979350ff667524386
Troy Chaplin <troy.chaplin@gmail.com>
2025-10-10 20:29:10 -0400

    finish: image pattern checks

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'aa6623e060abb38e1259');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '641c590ebdd15a2c8a05');
