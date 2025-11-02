be57b0b90f5d9ef4a3faca4731fdfcd6484a7383
Troy Chaplin <troy.chaplin@gmail.com>
2025-11-02 07:27:01 -0500

    change: meta error and warning styles

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '366a00e304231ebc8401');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => 'be91a2a01a624adb5474');
