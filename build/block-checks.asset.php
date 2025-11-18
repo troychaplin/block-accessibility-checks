2b8901c0252284a43c105e9e2f845365ad54e726
Troy Chaplin <troy.chaplin@gmail.com>
2025-11-17 20:21:58 -0500

    change: merge first heading check into hierarchy check

--- build/block-checks.asset.php
+++ build/block-checks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '09e4edf516d8020e525c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins'), 'version' => '9de695ddcf87da1be3f2');
