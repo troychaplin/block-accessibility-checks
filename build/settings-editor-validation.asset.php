18f26f06951ce1cabfb760607325731b44d646ea
Troy Chaplin <troy.chaplin@gmail.com>
2025-12-31 22:29:31 -0500

    change: settings page styles

--- build/settings-editor-validation.asset.php
+++ build/settings-editor-validation.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4d04e808ba875e159aee');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cde9edbfe3f42e2850bb');
--- build/settings-external-plugins.asset.php
+++ build/settings-external-plugins.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2461ac3e452d43ed3dc4');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '39af166e214c0cf83074');
